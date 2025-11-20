de7ef4d5563f3d184a2c93c4b8368b851ea9b79c
Angelo Michele Pizzi <154350496+passionext@users.noreply.github.com>
2025-11-20 17:45:11 +0100

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,26 +1,23 @@
 # -------------------------------
-# VPC NETWORK
+# VPC NETWORK and FIREWALL (No changes needed here)
 # -------------------------------
 resource "google_compute_network" "my_network" {
   name                    = "my-network"
   auto_create_subnetworks = false
 }
 
-# CKV2_GCP_18 FIX: Define a firewall rule for the network.
 resource "google_compute_firewall" "allow_internal_traffic" {
-  name     = "allow-internal-traffic"
-  network  = google_compute_network.my_network.name
+  name    = "allow-internal-traffic"
+  network = google_compute_network.my_network.name
   priority = 65534
-
   allow {
     protocol = "all"
   }
-
   source_ranges = ["10.0.0.0/8"]
 }
 
 # -------------------------------
-# SUBNET 1
+# SUBNET 1 (FIXED)
 # -------------------------------
 resource "google_compute_subnetwork" "subnet1" {
   name                     = "subnet-1"
@@ -28,16 +25,18 @@ resource "google_compute_subnetwork" "subnet1" {
   region                   = "us-central1"
   network                  = google_compute_network.my_network.id
   private_ip_google_access = true
-  enable_flow_logs         = true        # Correct attribute for VPC Flow Logs
+  # REMOVED: enable_flow_logs = true
+
   log_config {
+    enable               = true # <-- ADDED: Must be set to true to enable logs
     aggregation_interval = "INTERVAL_5_MIN"
-    flow_sampling       = 0.5
-    metadata            = "INCLUDE_ALL_METADATA"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
   }
 }
 
 # -------------------------------
-# SUBNET 2
+# SUBNET 2 (FIXED)
 # -------------------------------
 resource "google_compute_subnetwork" "subnet2" {
   name                     = "subnet-2"
@@ -45,10 +44,12 @@ resource "google_compute_subnetwork" "subnet2" {
   region                   = "us-central1"
   network                  = google_compute_network.my_network.id
   private_ip_google_access = true
-  enable_flow_logs         = true
+  # REMOVED: enable_flow_logs = true
+  
   log_config {
+    enable               = true # <-- ADDED: Must be set to true to enable logs
     aggregation_interval = "INTERVAL_5_MIN"
-    flow_sampling       = 0.5
-    metadata            = "INCLUDE_ALL_METADATA"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
   }
 }
