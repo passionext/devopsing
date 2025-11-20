34ad3e75faafeb2488d18c8d46e7795a1c076b84
Angelo Michele Pizzi <154350496+passionext@users.noreply.github.com>
2025-11-20 16:04:43 +0100

    Define firewall rule and update subnet configurations
    Added a firewall rule to allow internal traffic within the network and updated subnet configurations.

--- terraform/network.tf
+++ terraform/network.tf
@@ -6,32 +6,47 @@ resource "google_compute_network" "my_network" {
   auto_create_subnetworks = false
 }
 
+# CKV2_GCP_18 FIX: Define a firewall rule for the network.
+resource "google_compute_firewall" "allow_internal_traffic" {
+  name    = "allow-internal-traffic"
+  network = google_compute_network.my_network.name
+  priority = 65534 # Lower priority than default implicit rules (65535)
+
+  # Rule to allow communication within the network (often required for GKE, etc.)
+  allow {
+    protocol = "all"
+  }
+
+  # This targets all instances in the VPC
+  source_ranges = ["10.0.0.0/8"] 
+}
+
 # -------------------------------
 # SUBNET 1
 # -------------------------------
 resource "google_compute_subnetwork" "subnet1" {
-  name          = "subnet-1"
-  ip_cidr_range = "10.0.1.0/24"
-  region        = "us-central1"
-  network       = google_compute_network.my_network.id
+  name                     = "subnet-1"
+  ip_cidr_range            = "10.0.1.0/24"
+  region                   = "us-central1"
+  network                  = google_compute_network.my_network.id
   private_ip_google_access = true
   log_config {
-      enable = true
-      flow_sampling = 0.5 # Optional: specify a sampling rate
-    }
+    enable        = true
+    flow_sampling = 0.5
+  }
 }
 
 # -------------------------------
 # SUBNET 2
 # -------------------------------
 resource "google_compute_subnetwork" "subnet2" {
-  name          = "subnet-2"
-  ip_cidr_range = "10.0.2.0/24"
-  region        = "us-central1"
-  network       = google_compute_network.my_network.id
+  name                     = "subnet-2"
+  ip_cidr_range            = "10.0.2.0/24"
+  region                   = "us-central1"
+  network                  = google_compute_network.my_network.id
   private_ip_google_access = true
   log_config {
-    enable = true
-    flow_sampling = 0.5 # Optional: specify a sampling rate
+    enable        = true
+    flow_sampling = 0.5
   }
 }
