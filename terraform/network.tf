af53c4e83f1c257936a989a46c935425574f7a37
Angelo Michele Pizzi <154350496+passionext@users.noreply.github.com>
2025-11-20 18:02:36 +0100

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,37 +1,25 @@
-# -------------------------------
-# SUBNET 1 (FIXED LOG CONFIG SYNTAX)
-# -------------------------------
-resource "google_compute_subnetwork" "subnet1" {
-  name                     = "subnet-1"
-  ip_cidr_range            = "10.0.1.0/24"
-  region                   = "us-central1"
-  network                  = google_compute_network.my_network.id
-  private_ip_google_access = true
-
-  # CORRECTED: Use log_config block with parameters directly inside it
-  log_config {
-    enable               = true
-    aggregation_interval = "INTERVAL_5_MIN"
-    flow_sampling        = 0.5
-    metadata             = "INCLUDE_ALL_METADATA"
-  }
+# Must be defined first! (The VPC network)
+resource "google_compute_network" "my_network" {
+  name                    = "my-network"
+  auto_create_subnetworks = false
 }
 
-# -------------------------------
-# SUBNET 2 (FIXED LOG CONFIG SYNTAX)
-# -------------------------------
-resource "google_compute_subnetwork" "subnet2" {
-  name                     = "subnet-2"
-  ip_cidr_range            = "10.0.2.0/24"
-  region                   = "us-central1"
-  network                  = google_compute_network.my_network.id
-  private_ip_google_access = true
-
-  # CORRECTED: Use log_config block with parameters directly inside it
-  log_config {
-    enable               = true
-    aggregation_interval = "INTERVAL_5_MIN"
-    flow_sampling        = 0.5
-    metadata             = "INCLUDE_ALL_METADATA"
+# The firewall is also required to pass a previous check (CKV2_GCP_18)
+resource "google_compute_firewall" "allow_internal_traffic" {
+  name    = "allow-internal-traffic"
+  network = google_compute_network.my_network.name
+  priority = 65534
+  allow {
+    protocol = "all"
   }
+  source_ranges = ["10.0.0.0/8"]
+}
+
+# ... THEN the SUBNET definitions follow ...
+resource "google_compute_subnetwork" "subnet1" {
+    # ... your config ...
+}
+
+resource "google_compute_subnetwork" "subnet2" {
+    # ... your config ...
 }
